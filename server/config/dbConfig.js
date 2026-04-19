e9915ec3aac8b001fece72bebd757056139bed26
Rakib <rakibullslamrahul66@gmail.com>
2026-04-19 22:43:46 +0600

    2nd

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -4,12 +4,11 @@ const { seedDatabase } = require("../seeding");
 mongoose.connect(process.env.mongo_url);
 
 const db = mongoose.connection;
-
 db.on("connected", () => {
   console.log("Mongo Db Connection Successful");
 });
 
 db.on("error", () => {
-  console.log("Mongo Db Connection Failed");
+   console.log("Mongo Db Connection Failed");
 });
 
